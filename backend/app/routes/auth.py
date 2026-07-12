ae74633dc497a54fb3c5b48da75f37c1a1afbce1
Kathir Leo <kathirezhil2310288@ssn.edu.in>
2026-07-12 12:29:57 +0530

    authentication part tested

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,3 +1,135 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify, session
 
-auth = Blueprint("auth",__name__)
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app.extensions import db
+from app.models import User
+from app.utils.decorators import login_required 
+
+auth_bp = Blueprint("auth",__name__,url_prefix="/auth")
+
+
+@auth_bp.route("/test")
+def test():
+    return "Authentication blueprint working"
+
+
+@auth_bp.route("/register", methods=["POST"])
+def register():
+
+    data = request.get_json()
+    
+    if not data:
+        return jsonify({
+            "success": False,
+            "message": "Request body must be in JSON format"
+        }), 400
+    
+    # used get instead of data[] to avoid crash if any field is missing
+    email = data.get("email")
+    password = data.get("password")
+    confirm_password = data.get("confirm_password")
+    role = data.get("role")
+
+    if not all([email, password, confirm_password, role]):
+        return jsonify({
+            "success": False,
+            "message": "All fields are required."
+        }), 400
+    
+    if password != confirm_password: 
+        return jsonify({
+            "success": False,
+            "message": "Passwords do not match"
+        }), 400
+    
+    if role not in ["student", "companny"]:
+        return jsonify({
+            "success": False,
+            "message": "Invalid role"
+        }), 400
+    
+    if len(password) < 8:
+        return jsonify({
+            "success": False,
+            "message": "Password must be atleast 8 characters long"
+        }), 400
+    
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({
+            "success": False,
+            "message": "Email already registered"
+        }), 409 # 409 means conflict
+    
+    hashed_password = generate_password_hash(password)
+
+    user = User(email=email,password_hash=hashed_password,role=role)
+
+    try:
+        db.session.add(user)
+        db.session.commit()
+
+        return jsonify({
+            "success": True,
+            "message": "User registered successfully"
+        }), 201
+    
+    except Exception as e:
+        db.session.rollback()
+
+        return jsonify({
+            "success": False,
+            "message": "Registration failed",
+            "error": str(e)
+        }),500
+
+
+@auth_bp.route("/login", methods=["Post"])
+def login():
+    
+    data = request.get_json()
+
+    if not data:
+        return jsonify({
+            "success": False,
+            "message": "Request must be in JSON format"
+        }), 400
+    
+    email = data.get("email")
+    password = data.get("password")
+
+    if not all([email, password]):
+        return jsonify({
+            "success": False,
+            "message": "Email and password are required"
+        }), 400
+    
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password_hash,password):
+        return jsonify({
+            "success": False,
+            "message": "Invalid email or password"
+        }),401
+    
+    session["user_id"] = user.id
+    session["role"] = user.role
+
+    return jsonify({
+        "success": True,
+        "message": "Login successful",
+        "data": {
+            "id": user.id,
+            "email": user.email,
+            "role": user.role
+        }
+    }), 200
+
+@auth_bp.route("/protected")
+@login_required
+def protected():
+    return jsonify({
+        "success": True,
+        "message": "You are authenticated"
+        })
---
+++ backend/app/utils/decorators.py
@@ -0,0 +1,16 @@
+from functools import wraps
+
+from flask import session, jsonify
+
+def login_required(func):
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+
+        if "user_id" not in session:
+            return jsonify({
+                "success": False,
+                "message": "Authentication required"
+            }), 401
+        return func(*args,**kwargs)
+    return wrapper
